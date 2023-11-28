d4984b9924917cebe55244731063a7de6838659a
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-11-28 10:56:11 +0300

    day 2 2a

---
+++ node-discovery/src/test/java/training/spring/microservice/nodediscovery/NodeDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package training.spring.microservice.nodediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
